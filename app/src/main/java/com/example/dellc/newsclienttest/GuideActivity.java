031f2c54e4c4871418d8e5ab04097787088f651c
Jhonson2 <1750896420@qq.com>
2017-06-26 21:33:42 +0800

    创建GuideActivity引导页面+布局文件activity_guide

---
+++ app/src/main/java/com/example/dellc/newsclienttest/GuideActivity.java
@@ -0,0 +1,28 @@
+package com.example.dellc.newsclienttest;
+
+/**
+ * Created by dellc on 2017/6/26.
+ */
+
+public class GuideActivity extends BaseActivity {
+
+    @Override
+    public int getLyoutRes() {
+        return R.layout.activity_guide;
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
