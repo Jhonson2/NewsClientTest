59638c4a3b0211b8da853bb3487c23f112d33656
Jhonson2 <1750896420@qq.com>
2017-06-27 21:31:12 +0800

    初始化并显示ViewPager

--- app/src/main/java/com/example/dellc/newsclienttest/MainActivity.java
+++ app/src/main/java/com/example/dellc/newsclienttest/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.dellc.newsclienttest;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
+
+import com.example.dellc.newsclienttest.fragment.MainFragment01;
+import com.example.dellc.newsclienttest.fragment.MainFragment02;
+import com.example.dellc.newsclienttest.fragment.MainFragment03;
+import com.example.dellc.newsclienttest.fragment.MainFragment04;
+import com.example.dellc.newsclienttest.fragment.MainFragment05;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends BaseActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private ViewPager viewPager;
+    private RadioGroup radioGroup;
 
 
 
@@ -19,6 +29,30 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initView() {
+        radioGroup= (RadioGroup) findViewById(R.id.radio_group);
+        initViewPager();
+    }
+
+    private void initViewPager() {
+        viewPager= (ViewPager) findViewById(R.id.view_pager);
+        final List<Fragment> fragments=new ArrayList<>();
+        fragments.add(new MainFragment01());
+        fragments.add(new MainFragment02());
+        fragments.add(new MainFragment03());
+        fragments.add(new MainFragment04());
+        fragments.add(new MainFragment05());
+
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+        });
     }
 
     @Override
