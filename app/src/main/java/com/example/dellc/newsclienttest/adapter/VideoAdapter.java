a5558478950192136104102f98113ddd2508d768
Jhonson2 <1750896420@qq.com>
2017-06-29 23:49:49 +0800

    视频列表--显示RecylerView视频列表

---
+++ app/src/main/java/com/example/dellc/newsclienttest/adapter/VideoAdapter.java
@@ -0,0 +1,90 @@
+package com.example.dellc.newsclienttest.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.dellc.newsclienttest.R;
+import com.example.dellc.newsclienttest.bean.VideoEntity;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * 视频列表适配器
+ * Created by dellc on 2017/6/29.
+ */
+
+public class VideoAdapter extends RecyclerView.Adapter {
+    private Context context;
+
+    /**
+     * 列表数据集合
+     */
+    private List<VideoEntity.ResultBean> listDatas;
+
+
+    // 构造方法
+    public VideoAdapter(Context context, List<VideoEntity.ResultBean> listDatas) {
+        this.context = context;
+        this.listDatas = listDatas;
+    }
+
+    // 创建列表项, 返回ViewHolder
+    // ViewHolder: 避免每次都findViewById查找子控件，
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // 创建列表项
+        View itemView = LayoutInflater.from(context).inflate(
+                R.layout.item_video, parent, false);
+
+        // 创建ViewHolder，并返回
+        return new MyViewHolder(itemView);
+    }
+
+    // 刷新列表项中的子控件
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+        MyViewHolder holder = (MyViewHolder) viewHolder;
+
+        // 列表项对应的数据
+        VideoEntity.ResultBean video = listDatas.get(position);
+
+        // 显示列表项中的子控件
+            //1 显示视频标题
+        holder.tvVideoTitle.setText(video.getTitle());
+            //2 显示视频播放时长
+        String duration = video.getLength() / 60 + "分" + video.getLength() % 60 + "秒";
+        holder.tvVideoDuration.setText(duration);
+            //3 显示视频预览图片
+        Picasso.with(context).load(video.getCover()).into(holder.ivVideoImage);
+    }
+
+    // 有多少个列表项
+    @Override
+    public int getItemCount() {
+        return (listDatas == null) ? 0 : listDatas.size();
+    }
+
+    private class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView ivVideoImage;
+        private TextView tvVideoTitle;
+        private TextView tvVideoDuration;
+        private TextView tvPlayCount;
+
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            ivVideoImage = (ImageView) itemView.findViewById(R.id.iv_video_image);
+            tvVideoTitle = (TextView) itemView.findViewById(R.id.tv_video_title);
+            tvVideoDuration = (TextView) itemView.findViewById(R.id.tv_video_duration);
+            tvPlayCount = (TextView) itemView.findViewById(R.id.tv_play_count);
+        }
+    }
+}
--- app/src/main/java/com/example/dellc/newsclienttest/fragment/MainFragment03.java
+++ app/src/main/java/com/example/dellc/newsclienttest/fragment/MainFragment03.java
@@ -1,6 +1,10 @@
 package com.example.dellc.newsclienttest.fragment;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 import com.example.dellc.newsclienttest.R;
+import com.example.dellc.newsclienttest.adapter.VideoAdapter;
 import com.example.dellc.newsclienttest.base.URLManager;
 import com.example.dellc.newsclienttest.bean.VideoEntity;
 import com.google.gson.Gson;
@@ -10,11 +14,15 @@ import com.lidroid.xutils.http.ResponseInfo;
 import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.lidroid.xutils.http.client.HttpRequest;
 
+import java.util.List;
+
 /**
  * Created by dellc on 2017/6/27.
  */
 
 public class MainFragment03 extends BaseFragment {
+    private RecyclerView recyclerView;
+
     @Override
     protected int getLayoutRes() {
         return R.layout.fragment_main_03;
@@ -27,6 +35,11 @@ public class MainFragment03 extends BaseFragment {
 
     @Override
     protected void initView() {
+        recyclerView = (RecyclerView) mRootView.findViewById(R.id.recycler_view);
+    }
+
+    @Override
+    protected void initData() {
         getVideoDatas();
 
     }
@@ -38,7 +51,7 @@ public class MainFragment03 extends BaseFragment {
         HttpUtils utils=new HttpUtils();
         utils.send(HttpRequest.HttpMethod.GET, URLManager.VideoURL, new RequestCallBack<String>() {
 
-            @Override
+            @Override// 请求成功
             public void onSuccess(ResponseInfo<String> responseInfo) {
                 String json = responseInfo.result;
                 System.out.println("----服务器返回的json数据:" + json);
@@ -51,9 +64,15 @@ public class MainFragment03 extends BaseFragment {
                 VideoEntity videoEntity=gson.fromJson(json,VideoEntity.class);
                 System.out.println("----解晰json数据:" +videoEntity.getResult().size()+"个视频");
 
+                // 列表显示的数据
+                List<VideoEntity.ResultBean> listDatas = videoEntity.getResult();
+
+                // （3）显示列表(RecyclerView: 列表，网格，瀑布流)
+                // RecyclerView: 列表数据，列表项布局， 适配器
+               showRecyclerView(listDatas);
             }
 
-            @Override
+            @Override        // 请求失败
             public void onFailure(HttpException error, String msg) {
                 error.printStackTrace();
                 System.out.println("----服务器返回的:" + error);
@@ -61,9 +80,14 @@ public class MainFragment03 extends BaseFragment {
             }
         });
     }
-
-            @Override
-    protected void initData() {
+    /** 显示列表RecyclerView */
+    private void showRecyclerView(List<VideoEntity.ResultBean> listDatas) {
+        // 设置列表布局管理器-----LinearLayoutManager线性布局
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        VideoAdapter videoAdapter = new VideoAdapter(getContext(), listDatas);
+        recyclerView.setAdapter(videoAdapter);
 
     }
+
+
 }
