8be41038c9d52330fceb9354fc05680a536bfc6c
Jhonson2 <1750896420@qq.com>
2017-06-29 22:12:45 +0800

    显示返回图标+退出操作

--- app/src/main/java/com/example/dellc/newsclienttest/NewsDetailActivity.java
+++ app/src/main/java/com/example/dellc/newsclienttest/NewsDetailActivity.java
@@ -1,10 +1,13 @@
 package com.example.dellc.newsclienttest;
 
-import android.provider.Settings;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.example.dellc.newsclienttest.bean.NewsEntity;
+
 
 /**
  * Created by dellc on 2017/6/29.
@@ -30,6 +33,18 @@ public class NewsDetailActivity extends BaseActivity {
 
     private void initWebView() {
         webView = (WebView) findViewById(R.id.web_view);
+        // 支持javascript脚本
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        // 禁止使用外部的浏览器打开网页
+        webView.setWebViewClient(new WebViewClient(){
+            @Override  // 网页加载完成
+            public void onPageFinished(WebView view, String url) {
+                System.out.println("------onPageFinished");
+                super.onPageFinished(view, url);
+            }
+        });
+
 
     }
 
@@ -40,10 +55,25 @@ public class NewsDetailActivity extends BaseActivity {
                 getIntent().getSerializableExtra("news");
 
         String newUrl = newsBean.getUrl();
-        System.out.println("----------"+newUrl);
+        System.out.println("----------" + newUrl);
         webView.loadUrl(newUrl);        // 显示新闻网页数据
+
+        // 显示标题栏左上角的返回图标
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // 显示标题栏
         getSupportActionBar().setTitle(newsBean.getTitle());
     }
+
+
+       @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            if (item.getItemId() == android.R.id.home) {
+                finish();   // 标题栏左上角的返回按钮，退出当前界面
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+
 }
